d106443cd9a0e814af56005697aa885111ddb1a8
Imsan2000 <isanatnama@gmail.com>
2018-09-12 11:20:22 +0200

    läste in användares namn

--- src/Läxa1.java
+++ src/Läxa1.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+
 public class Läxa1 {
-public static void main(String[] args) {
-	Scanner Input = new  Scanner(System.in);
-}
+	public static void main(String[] args) {
+		Scanner Input = new Scanner(System.in);
+		System.out.println("Vad heter du?");
+		
+		String name = Input.nextLine();
+	}
 }
